d811979cf7557328c275b1e7a33e65986346f3d5
William McGonagle <wpmcgonagle@gmail.com>
2023-09-28 13:32:56 -0400

    Finally Fixed Index

--- src/pages/events.js
+++ src/pages/events.js
@@ -76,7 +76,7 @@ function ProgramsPage({location}) {
   return (
     <Layout location={location} crumbLabel="programs" >
       <h1 className="hidden">Events</h1>
-      <a href="webcal://gtowntech.org/api/events" className="fixed bottom-4 right-4 w-20 aspect-square bg-primary flex justify-center items-center rounded-3xl hover:shadow-xl transition-shadow z-999">
+      <a href="webcal://gtowntech.org/api/events" className="fixed bottom-4 right-4 w-20 aspect-square bg-primary flex justify-center items-center rounded-3xl hover:shadow-xl transition-shadow z-50">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-12 h-12 text-white">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
         </svg>
